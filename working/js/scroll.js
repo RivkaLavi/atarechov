a4dbebb7a0147fdece9a861f517d002039ebae41
Pavel Suchman <pavel.suchman@gmail.com>
2018-05-02 19:38:58 +0300

    Call cellResponsive function AFTER template render

--- working/js/scroll.js
+++ working/js/scroll.js
@@ -7,16 +7,4 @@ function bizInfoScroller(){
     else {
        element.style.cssText += "position: relative; top: 0px; bottom: auto; right: auto; left: 10px;";  
      } 
-   }
-
-   function cellResponsive(desiredXPosition) {
-    var element = document.getElementById("business_info_wrapper")
-    if (desiredXPosition.matches) { // If media query matches
-      element.style.cssText +="position: absolute; top: 0px; right: 0px;"
-    } else {
-      element.style.cssText += "position: relative; top: 0px; bottom: auto; right: auto; left: 10px;"; 
-    }
 }
-var desiredXPosition = window.matchMedia("(max-width: 768px)")
-cellResponsive(desiredXPosition) // Call listener function at run time
-desiredXPosition.addListener(cellResponsive) // Attach listener function on state changes
--- working/render_firebase.js
+++ working/render_firebase.js
@@ -130,6 +130,16 @@ function changeContent(error, options, response) {
   function myFunction() {
     document.getElementById("map").style.overflow = "scroll";
   }
+
+  function cellResponsive(desiredXPosition) {
+    var element = document.getElementById("business_info_wrapper")
+    if (desiredXPosition.matches) { // If media query matches
+      element.style.cssText ="position: absolute; top: 0px; right: 0px;"
+    } else {
+      element.style.cssText = "position: relative; top: 0px; bottom: auto; right: auto; left: 10px;";
+    }
+  }
+  var desiredXPosition = window.matchMedia("(max-width: 768px)")
 }
 
 var observer = new MutationObserver(changeContent);
